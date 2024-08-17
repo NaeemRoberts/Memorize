0b81a44659a3efe9dfdb3743fff6eba2ebb0771e
Naeem Roberts <120337023+NaeemRoberts@users.noreply.github.com>
2024-08-17 19:10:15 -0400

    Initial Commit: Creating of card view and isFaceUp

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Naeem Roberts on 8/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding() // View Modifier, function that will take the view and returns a new view
+        
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius:12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius:12)
+                    .strokeBorder(lineWidth: 3)
+                Text("🙂‍↔️").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius:12)
+                    
+            }
+        }}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Naeem Roberts on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
