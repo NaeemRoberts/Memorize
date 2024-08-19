73c73267ff930602bdccaa22fea1699bd23a270c
Naeem Roberts <naeemroberts@proton.me>
2024-08-19 01:25:52 -0400

    Toggleable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
+            CardView(isFaceUp: false)
+            CardView()
             CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
         }
         .padding() // View Modifier, function that will take the view and returns a new view
         
@@ -22,21 +22,23 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius:12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius:12)
-                    .strokeBorder(lineWidth: 3)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("🙂‍↔️").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius:12)
-                    
+                base.fill()
             }
-        }}
+        }
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
+        }
 }
 
 
